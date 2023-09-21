02c411490d1a782dbe5402f3db70d257538f7111
ariyoujustcode <arimarine23@gmail.com>
2023-09-21 12:56:21 -0500

    finished portfolio section

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -18,19 +18,19 @@ function Portfolio() {
           </a>
         </div>
         <div className='port-two'>
-          <a href='#'>
-            <img src='' alt=''></img>
+          <a href='https://apps.apple.com/us/app/easeio/id1609934534' target='_blank'>
+            <img id='ease' src='images/ease-io-marine-studios.webp' alt='EaseIO iOS app'></img>
           </a>
-          <a href='#'>
-            <img src='' alt=''></img>
+          <a href='https://lucidity.vercel.app/' target='_blank'>
+            <div id='lucid'>Lucidity</div>
           </a>
-          <a href='#'>
-            <img src='' alt=''></img>
+          <a href='https://curb-herb-duluth.vercel.app/' target='_blank'>
+            <img id='curb' src='images/marijuana-g3dad059d3_1280.png' alt='CurbHerb Delivery Services'></img>
           </a>
         </div>
         <div className='port-three'>
-          <a href='#'>
-            <img src='' alt=''></img>
+          <a href='https://intuit.ariyoujustcode.repl.co/index.html' target='_blank'>
+            <div id='intuit'>Intuit</div>
           </a>
         </div>
       </div>
